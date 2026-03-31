37ad443f6e9180c066b422acc3fdedf1c86da6af
Samir Jadhav <jadhavsamir644@gmail.com>
2026-03-31 12:05:33 +0530

    modified

--- demo/src/test/java/com/example/hello.java
+++ demo/src/test/java/com/example/hello.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class hello {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world panchal");
     }
     
 }
